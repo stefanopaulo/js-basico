c9c929f663f06da967363d83d324fe889eef3802
Stefano Paulo <stefanopaulo47@gmail.com>
2020-07-02 10:43:41 -0300

    comentarios

---
+++ aula004/index.js
@@ -0,0 +1,15 @@
+// Comentário de uma linha
+console.log('Olá mundo!'); // Aqui tem outro comentário
+console.log('Linha 1');
+// Comentário
+// console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4');
+// Vai ser ignorado
+
+/**
+ * Comentário
+ * de
+ * multiplas
+ * linhas
+ */
