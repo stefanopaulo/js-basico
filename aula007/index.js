02c9d327da1bebcbb1be7f97c66e4338d79b33d9
Stefano Paulo <stefanopaulo47@gmail.com>
2020-07-07 11:28:18 -0300

    constante com const

---
+++ aula007/index.js
@@ -0,0 +1,17 @@
+// CONSTANTES
+
+const primeiroNumero = '5';
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+const resultadoTriplicado = resultado * 3;
+
+console.log(primeiroNumero);
+console.log(segundoNumero);
+console.log(resultado);
+console.log(resultadoDuplicado);
+console.log(resultadoTriplicado);
+
+console.log();
+console.log(typeof primeiroNumero);
+console.log(typeof segundoNumero);
