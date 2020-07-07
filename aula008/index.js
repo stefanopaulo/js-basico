eafaa075d453db9458e14137d0dbfd8f977e1294
Stefano Paulo <stefanopaulo47@gmail.com>
2020-07-07 14:13:03 -0300

    exercicio com const e let

--- aula008/index.js
+++ aula008/index.js
@@ -8,4 +8,4 @@ const anoNascimento = 2020 - idade;
 
 console.log(`${nome} ${sobrenome} tem ${idade} anos, pesa ${peso} kg`);
 console.log(`tem ${altura} e seu IMC é de ${imc}`)
-console.log(`${nome} nasceu em ${anoNascimento}`)
+console.log(`${nome} nasceu em ${anoNascimento}.`)
