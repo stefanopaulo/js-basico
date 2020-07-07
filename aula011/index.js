ad72a9f2b892670cd068ae69463fb577f44909c6
Stefano Paulo <stefanopaulo47@gmail.com>
2020-07-07 15:09:44 -0300

    operadores aritimeticos, incremento e atribuicao

---
+++ aula011/index.js
@@ -0,0 +1,33 @@
+/*Aritiméticos
+  + Adição / Concatenação
+  - * / 
+  ** Potenciação
+  % Resto da divisão
+*/
+
+const num1 = 10;
+const num2 = Number('5');
+
+console.log(num1 + num2);
+
+let contador = 1;
+contador++; // incremento
+console.log(contador);
+
+contador--; // decremento
+console.log(contador);
+
+contador += 5; // contador = contador + 5
+console.log(contador);
+
+contador -= 3; // contador = contador - 3
+console.log(contador);
+
+contador *= 7;
+console.log(contador);
+
+contador /= 3;
+console.log(contador);
+
+contador **= 2;
+console.log(contador);
