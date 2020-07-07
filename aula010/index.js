6b0fd028aaa413e1bf418520a9bab9bc7f8400ca
Stefano Paulo <stefanopaulo47@gmail.com>
2020-07-07 14:35:15 -0300

    tipos de dados primitivos

---
+++ aula010/index.js
@@ -0,0 +1,15 @@
+// Tipos de dados primitivos
+
+const nome = 'Stefano'; // string
+const num1 = 10; // number
+const num2 = 10.52; // number
+let nomeAluno; // undefined = não aponta para local nenhum na memória
+const sobrenomeAluno = null; // null = não aponta para local nenhum na memória
+const aprovado = true; // Boolean = true, false
+
+console.log(typeof nome);
+console.log(typeof num1);
+console.log(typeof num2);
+console.log(typeof nomeAluno);
+console.log(typeof sobrenomeAluno);
+console.log(typeof aprovado);
